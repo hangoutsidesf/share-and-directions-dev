5f6ec4abd2cfd68e418cc4ab37e5a0ca9b0001c5
Justin <jvcruz@ucdavis.edu>
2018-04-04 01:25:31 -0700

    added react-device-detect features to client

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.parkId = this.props.match.params.parkId || 'home page';
-		console.log(this.props.match);
+		console.log(isBrowser);
 	}
 	render() {
 		return (
 		  <div>
-		    <h1>{ this.parkId }</h1>
+		    <h1>Endpoint: { this.parkId }</h1>
+		    <h1>Is mobile? { String(isMobile) }</h1>
+		    <h1>Is browser? { String(isBrowser) }</h1>
 		  </div>
 		);
 	}
