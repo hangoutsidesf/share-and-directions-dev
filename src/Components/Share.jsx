fec2bf39accf419a396d9619ce43421f94892eee
Justin <jvcruz@ucdavis.edu>
2018-04-05 00:25:42 -0700

    minor fixes

--- src/Components/Share.jsx
+++ src/Components/Share.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import copy from 'copy-to-clipboard';
 import { isBrowser, isMobile } from 'react-device-detect';
 
+const handler = () => {
+	console.log(window.location);
+	const url = `${window.location.origin}${window.location.pathname}`;
+
+	if(isBrowser) {
+		const display = document.querySelectorAll('.copied');
+		copy(url);
+
+		display.forEach(node => {
+			node.textContent = 'Link copied.';
+		});
+	} else {
+		// insert mobile stuff here
+	}
+};
+
 export default props => {
-
-	const handler = event => {
-		const url = `${window.location.origin}/${props.parkId}`;
-
-		if(isBrowser) {
-			const display = document.querySelectorAll('.copied');
-			copy(url);
-
-			display.forEach(node => {
-				node.textContent = 'Link copied.';
-			});
-		} else {
-			// insert mobile stuff here
-		}
-	};
-
-
-	return <button onClick={handler}>Share</button>;
+	if(isBrowser) {
+		return <button onClick={handler}>Share</button>;
+	} else {
+		return (
+			<form action="#">
+				<button>Share</button>
+			</form>
+		);
+	}
 }
