0694e98e4f8d2baad68558eaa204600cd202f970
Justin <jvcruz@ucdavis.edu>
2018-04-04 18:59:42 -0700

    copies to clipboard now

--- src/Components/Share.jsx
+++ src/Components/Share.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import copy from 'copy-to-clipboard';
+import { isBrowser, isMobile } from 'react-device-detect';
 
 export default props => {
 
 	const handler = event => {
-		console.log(window.location.origin, props.parkId);
+		const url = `${window.location.origin}/${props.parkId}`;
+
+		if(isBrowser) {
+			const display = document.querySelectorAll('.copied');
+			copy(url);
+
+			
+		} else {
+			// insert mobile stuff here
+		}
 	};
 
 
