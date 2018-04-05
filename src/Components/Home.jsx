11f0ab0c576f1a16dd7002806370bbd72e67e788
Justin <jvcruz@ucdavis.edu>
2018-04-05 04:52:55 -0700

    added homepage

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+	<div>
+		<h1>Instructions</h1> 
+		<h2>
+		Try to type an endpoint into the URL (this uses '/:parkId'). The real function of this dev app
+		is to test out the Share and Get Directions buttons which has no place in '/'.
+		</h2>
+	</div>
+);
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './Components/App';
+import Home from './Components/Home';
  
 const Main = () => (
   <BrowserRouter>
     <Switch>
-    	<Route exact path="/" component={App} />
+    	<Route exact path="/" component={Home} />
     	<Route exact path="/:parkId" component={App} />
     </Switch>
   </BrowserRouter>
